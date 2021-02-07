fe57a0c9bae047b444b067fd576e68b300bb02db
Tunc <tuncutku10@gmail.com>
2021-02-06 17:17:16 -0800

    finalize position and report

--- src/environment/user_activities/position.py
+++ src/environment/user_activities/position.py
@@ -1,55 +1,55 @@
 from pydantic.dataclasses import dataclass
 from typing import Union
 import pandas as pd
+import yfinance as yf
 
 security_type_map = {}
 
 
-@dataclass
 class Position:
-
-    name: Union[str, None]
-    symbol: Union[str, None]
-    security_type: Union[str, None]
-    currency: Union[str, None]
+    def __init__(self, symbol, name, security_type, currency, quantity, market_cap):
+        self.symbol = symbol
+        self.name = name
+        self.security_type = security_type
+        self.currency = currency
+        self.quantity = quantity
+        self.market_cap = market_cap
 
     def orders(self, orders):
         pass
 
     @property
-    def quantity(self):
-        total_position_quantity = sum(
-            [
-                order.filledQuantity
-                if order.side == "Buy"
-                else order.filledQuantity * -1
-                for order in orders
-            ]
-        )
-        pass
-
-    @property
     def open(self) -> bool:
-        pass
+        return True if self.quantity != 0 else False
 
     def generate_trades(self, orders):
         pass
 
     @classmethod
-    def from_dict(
-        cls,
-        symbol: str,
-        shortName: str,
-        quoteType: str,
-        currency: str,
-    ):
-        return cls(shortName, symbol, quoteType, currency)
+    def from_df(cls, symbol: str, df: pd.DataFrame):
+        symbol_info = yf.Ticker(symbol).info
+
+        quote = (
+            yf.Ticker(symbol)
+            .history(period="1m", interval="1m")["Close"]
+            .values.tolist()
+        )
+        total_pos_quantity = df["Side amount"].sum()
+        mkt_cap = round(quote[0] * total_pos_quantity, 2) if quote else None
+
+        return cls(
+            symbol,
+            symbol_info.get("shortName", None),
+            symbol_info.get("quoteType", None),
+            symbol_info.get("currency", None),
+            total_pos_quantity,
+            mkt_cap,
+        )
 
     @staticmethod
     def position_attributes_map():
         return {
-            "Symbol": "symbol",
             "Name": "name",
-            "Type": "security_type",
             "Currency": "currency",
+            "Market Cap": "market_cap",
         }
--- src/reports/portfolio_reports.py
+++ src/reports/portfolio_reports.py
@@ -9,34 +9,35 @@ import inspect
 class PortfolioReport:
     def __init__(self, portfolio: Portfolio, start_date=None, end_date=None):
         self.underlying_portfolio = portfolio
+        self.positions = self.get_positions()
 
-    def generate_position_df(self):
+    def generate_order_df(self):
         order_list = Order.query.filter_by(
             portfolio_id=self.underlying_portfolio.id
         ).all()
         return pd.DataFrame.from_records([order.to_dict() for order in order_list])
 
     @property
-    def positions(self):
-        df = self.generate_position_df()
+    def port_mkt_cap():
+        pass
 
+    def get_positions(self):
         position_list = list()
-        for symbol in df.symbol.unique():
-            symbol_df = df.loc[df["symbol"] == symbol]
+        df = self.generate_order_df()
+        if not df.empty:
+            df["Side multiplier"] = df["side"].apply(lambda x: 1 if x == "Buy" else -1)
+            df["Side amount"] = df["Side multiplier"] * df["quantity"]
 
-            symbol_info = yf.Ticker(symbol).info
-            position_attr = [
-                "symbol",
-                "shortName",
-                "quoteType",
-                "currency",
-            ]
-            position_attr_dict = dict()
-            for attr in position_attr:
-                position_attr_dict[attr] = symbol_info.get(attr, None)
-            position_list.append(Position.from_dict(**position_attr_dict))
+            position_list = list()
+            for symbol in df.symbol.unique():
+                symbol_df = df.loc[df["symbol"] == symbol]
+                position_list.append(Position.from_df(symbol, df))
         return position_list
 
     @property
     def position_attributes_map(self):
         return Position.position_attributes_map()
+
+    @property
+    def total_portfolio_value(self):
+        pass
