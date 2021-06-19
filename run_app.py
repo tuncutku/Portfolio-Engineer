c03a6059b92269566c0f14c9fdbf971580b81c95
Tunc <tuncutku10@gmail.com>
2021-06-19 10:24:14 -0700

    edit github actions

--- run_app.py
+++ run_app.py
@@ -7,7 +7,8 @@ from src import create_app
 
 
 load_dotenv()
+port = int(os.environ.get("PORT", 5001))
 app = create_app(os.getenv("FLASK_CONFIG"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=port)
